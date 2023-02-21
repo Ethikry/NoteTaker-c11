e54f391c5ab2776e9b9962b50be254e9f5641ff8
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-02-21 03:39:50 -0700

    Added put method to edit notes and delete method, full app working

--- 11-Challenge/Develop/server.js
+++ 11-Challenge/Develop/server.js
@@ -10,9 +10,13 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+// app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
 
-app.get('/index', (req, res) =>
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
@@ -20,11 +24,25 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+// Function to edit notes
+const editNote = (notes) => {
+  // fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+  //   if (err) {
+  //     console.error(err);
+  //   };
+  // });
+  fs.writeFile(
+    './db/db.json',
+    JSON.stringify(notes, null, 4),
+    (writeErr) =>
+      writeErr
+        ? console.error(writeErr)
+        : console.info('Successfully updated notes!')
+  );
+};
+
 // GET request for notes
-app.get('/api/notes', (req, res) => {
-    // Send a message to the client
-    res.status(200).json(`${req.method} request received to get notes`);
-  
+app.get('/api/notes', (req, res) => {  
     // Log our request to the terminal
     console.info(`${req.method} request received to get notes`);
 
@@ -32,9 +50,8 @@ app.get('/api/notes', (req, res) => {
         if (err) {
           console.error(err);
         } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-          console.log(parsedNotes);
+          // Parse data and send result
+          res.json(JSON.parse(data));
         }
     });
 });
@@ -53,7 +70,7 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
       };
   
       // Obtain existing notes
@@ -68,14 +85,7 @@ app.post('/api/notes', (req, res) => {
           parsedNotes.push(newNote);
   
           // Write updated reviews back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated notes!')
-          );
+          editNote(parsedNotes);
         }
       });
   
@@ -90,6 +100,52 @@ app.post('/api/notes', (req, res) => {
       res.status(500).json('Error in posting note');
     }
   });
+
+app.put("/api/notes/:id", (req, res) => {
+  const noteToEditId = req.params.id;
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let notes = JSON.parse(data);
+      let noteToEdit = notes.find((note) => note.id === noteToEditId);
+
+      if (noteToEdit) {
+        let editedNote = {
+          title: req.body.title,
+          text: req.body.text,
+          id: noteToEdit.id,
+        };
+        let targetIndex = notes.indexOf(noteToEdit);
+        notes.splice(targetIndex, 1, editedNote);
+
+        res.sendStatus(204);
+        editNote(notes);
+        res.json(notes);
+      } else {
+        res.sendStatus(404);
+      };
+    };
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const noteToDeleteId = req.params.id;
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let notes = JSON.parse(data);
+      let noteToDelete = notes.find((note) => note.id === noteToDeleteId);
+      let targetIndex = notes.indexOf(noteToDelete);
+      notes.splice(targetIndex, 1);
+      res.sendStatus(204);
+      editNote(notes);
+      console.log(`Note deleted. Deleted note title: ${noteToDelete.title}`);
+    }
+  })
+});
 
 app.listen(PORT, () =>
   console.log(`Note_Taker app listening at http://localhost:${PORT}`)
